0c9386a19cd1b5ec9dfcebb6cc85b935e51f2c38
DenisGlod <denis.glod@mail.ru>
2019-11-28 00:56:48 +0300

    Small fix

--- customs-duty/server/src/main/java/com/server/dao/HibernateUtil.java
+++ customs-duty/server/src/main/java/com/server/dao/HibernateUtil.java
@@ -14,6 +14,7 @@ public class HibernateUtil {
 		try {
 			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 		} catch (Exception e) {
+			e.printStackTrace();
 			StandardServiceRegistryBuilder.destroy(registry);
 		}
 	}
